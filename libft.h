e798a1b6f76556678cc23f2a0951f2eb5f89ba11
Edith Puclla <epuclla@e1z3r8p20.42.us.org>
2020-03-02 17:23:07 -0800

    fixing libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: epuclla <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 09:57:51 by epuclla           #+#    #+#             */
-/*   Updated: 2020/03/02 14:11:06 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/03/02 16:23:26 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,9 @@ char *ft_strcpy(char *dest, const char *src);
 char *ft_strncpy(char * dst, const char * src, size_t len);
 char *ft_strcat(char *str1, const char *str2);
 char *ft_strncat(char *str1, const char *str2, size_t n);
-size_t ft_strlcat(char *dest, const char *src, size_t size);
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+
 char *ft_strchr(const char *s, int c);
 char *ft_strrchr(const char *s, int c);
 char *ft_strstr(const char *haystack, const char *needle);
