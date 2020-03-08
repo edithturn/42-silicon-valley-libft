6fcbd9c03e81dc6785bd0c05aa4f5a266cbde025
Edith Puclla <epuclla@e1z2r7p10.42.us.org>
2020-03-07 22:02:46 -0800

    fixing ft_memccpy.c

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -17,20 +17,18 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	size_t			i;
 	unsigned char	*udst;
 	unsigned char	*usrc;
+	unsigned int	uc;
 	
 	i = 0;
 	udst = (unsigned char *)dst;
 	usrc = (unsigned char *)src;
-	uc = (unsigned char *)c;
+	uc = (unsigned char)c;
 
-	while (n > 0)
+	while (n > i)
 	{
 		udst[i] = usrc[i];
-		if (usrc[i] == uc);
-		{
-			udst = usdt + 1;
-			return (udst);
-		}
+		if (usrc[i] == uc)
+			return (udst + i + 1);
 		i++;
 	}
 	return (NULL);
