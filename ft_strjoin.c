75780f8e4243eb8cbf8e7c190c380793886154b9
edithturn <edithpuclla20@gmail.com>
2020-03-20 01:04:28 -0700

    fixing join

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -12,19 +12,16 @@
 
 #include "libft.h"
 
-char * ft_strjoin(char const *s1, char const *s2)
+char *ft_strjoin(char const *s1, char const *s2)
 {
-
 	char *ptr;
 
-	if (!s1 || !s2)
+	if (s1 == '\0' || s2 == '\0')
 		return NULL;
-	ptr = ft_strnew(ft_strlen(s1) + ft_str_len(s2) - 1);
+	ptr = ft_strnew(ft_strlen(s1) + ft_strlen(s2) - 1);
 	if (ptr == NULL)
 		return NULL;
-
-	ft_strcpy(ptr, s1)
+	ft_strcpy(ptr, s1);
 	ft_strcat(ptr, s2);
-
 	return(ptr);
 }
--- libft.h
+++ libft.h
@@ -33,7 +33,7 @@ char *ft_strcpy(char *dest, const char *src);
 char *ft_strncpy(char * dst, const char * src, size_t len);
 char *ft_strcat(char *str1, const char *str2);
 char *ft_strncat(char *str1, const char *str2, size_t n);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t n);
 char *ft_strchr(const char *s, int c);
 char *ft_strrchr(const char *s, int c);
 char *ft_strstr(const char *haystack, const char *needle);
@@ -63,6 +63,9 @@ void ft_putnbr(int nbr);
 void ft_putnbr_fd(int nbr, int fd);
 void ft_putendl(char const *s);
 void ft_putendl_fd(char const *s, int fd);
+
+char *ft_strjoin(char const *s1, char const *s2);
+
 
 
 
