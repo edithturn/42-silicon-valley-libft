2a51586e539016d017f88615d5e5862a428b1bf8
edithturn <edithpuclla20@gmail.com>
2020-03-29 23:28:30 -0700

    fixing strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -15,9 +15,13 @@
 int	ft_strcmp(const char *s1, const char *s2)
 {
 	int i;
+	unsigned char	*us1;
+	unsigned char	*us2;
 
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+	us1 = (unsigned char *)s1;
+	us2 = (unsigned char *)s2;
+	while (us1[i] == us2[i] && us1[i] != '\0' && us2[i] != '\0')
 		i++;
-	return (s1[i] - s2[i]);
+	return (us1[i] - us2[i]);
 }
