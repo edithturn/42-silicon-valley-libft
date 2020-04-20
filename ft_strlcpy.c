0d66ed40f0cbc37845d570f948034f417f64a08c
edithturn <edithpuclla20@gmail.com>
2020-04-19 18:04:57 -0700

    fixing strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: edith <edith@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 10:06:43 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/18 16:20:30 by edith            ###   ########.fr       */
+/*   Updated: 2020/04/19 17:58:12 by edith            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
+	size_t size_tmp;
 
 	if(!dst && !src)
 		return (0);
 	if (size == 0)
 		return (ft_strlen(src));
-	size = ft_strlen(src);
-	if(size < size)
+	size_tmp = ft_strlen(src);
+	if(size_tmp < size)
 		ft_strcpy(dst, src);
 	else
 		ft_strncpy(dst, src, size - 1);	
 	dst[size - 1] = '\0';
-	return (size);
+	return (size_tmp);
 }
