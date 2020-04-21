60ace60bc9614e0312488b8db5493227cf112be4
edithturn <edithpuclla20@gmail.com>
2020-04-21 15:55:38 -0700

    fixing pt

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,13 +6,13 @@
 /*   By: edith <edith@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 01:07:10 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/21 15:49:19 by edith            ###   ########.fr       */
+/*   Updated: 2020/04/21 15:55:28 by edith            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		ft_putstr_fd(char *s, int fd)
+void		ft_putstr_fd(char const *s, int fd)
 {
 	if (!s)
 		return;
