2afbdfef9b1d0cb11a3b1e32838e6f8b0c68490a
edithturn <edithpuclla20@gmail.com>
2020-04-21 17:37:18 -0700

    removing unused variable

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: edith <edith@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/17 10:29:09 by edith             #+#    #+#             */
-/*   Updated: 2020/04/21 17:31:40 by edith            ###   ########.fr       */
+/*   Updated: 2020/04/21 17:36:57 by edith            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ Parameters: #1. The beginning of the list.
 
 t_list			*ft_lstlast(t_list *lst)
 {
-	t_list *next;
-
 	while(lst->next != NULL)
 		lst = lst->next;
 	return (lst);
