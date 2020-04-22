8601a39a3f7a76d62e6375f06d8b7dfda301053a
edithturn <edithpuclla20@gmail.com>
2020-04-21 17:41:21 -0700

    addfding null validation

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: edith <edith@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/17 10:29:09 by edith             #+#    #+#             */
-/*   Updated: 2020/04/21 17:36:57 by edith            ###   ########.fr       */
+/*   Updated: 2020/04/21 17:40:41 by edith            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ Parameters: #1. The beginning of the list.
 
 t_list			*ft_lstlast(t_list *lst)
 {
+	if (lst == NULL)
+		return NULL;
 	while(lst->next != NULL)
 		lst = lst->next;
 	return (lst);
