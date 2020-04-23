8bb5247c6039e026e4f78666a76c2acec917d1ff
edithturn <edithpuclla20@gmail.com>
2020-04-23 00:04:14 -0700

    norminette for ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,25 +6,25 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/17 22:30:53 by edith             #+#    #+#             */
-/*   Updated: 2020/04/22 21:32:44 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/23 00:03:33 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-Iterates the list ’lst’ and applies the function ’f’ to the content 
-of each element. Creates a new list resulting of the successive 
-applications of the function ’f’. The ’del’ function is used to
-delete the content of an element if needed.
-Parameters:
-#1. The adress of a pointer to an element.
-#2. The adress of the function used to iterate on the list.
-#3. The adress of the function used to delete the content 
-of an element if needed.
+** Iterates the list ’lst’ and applies the function ’f’ to the content
+** of each element. Creates a new list resulting of the successive
+** applications of the function ’f’. The ’del’ function is used to
+** delete the content of an element if needed.
+** Parameters:
+** #1. The adress of a pointer to an element.
+** #2. The adress of the function used to iterate on the list.
+** #3. The adress of the function used to delete the content
+** of an element if needed.
 */
 
-t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*next;
 	t_list	*new_node;
