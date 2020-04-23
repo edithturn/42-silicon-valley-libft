898d4d4c77597c44cacc5d25ef5064ca442bb766
edithturn <edithpuclla20@gmail.com>
2020-04-23 14:39:12 -0700

    refactor itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/22 20:13:31 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/23 14:33:36 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/23 14:39:04 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_itoa(int n)
 	if (n == 0)
 	{
 		ptr = "0";
-		return (ptr);
+		return (ft_strdup(ptr));
 	}
 	num = (n < 0) ? -n : n;
 	lenght = (n < 0) ? 1 : 0;
@@ -30,7 +30,7 @@ char	*ft_itoa(int n)
 		num = num / 10;
 		lenght++;
 	}
-	if (!(ptr = ft_strnew(lenght + 1)))
+	if (!(ptr = ft_strnew(lenght)))
 		return (NULL);
 	num = (n < 0) ? -n : n;
 	while (num > 0)
