407f651f7bac3658dbcae89af32fc4951d220773
edithturn <edithpuclla20@gmail.com>
2020-04-22 23:38:20 -0700

    norminette

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,24 +6,24 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/22 20:14:01 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/22 20:14:03 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/22 23:37:43 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-Adds the element ’new’ at the end of the list.
-Parameters:
-#1. The address of a pointer to the first link of a list.
-#2. The address of a pointer to the element to be added to the list.
+**Adds the element ’new’ at the end of the list.
+** Parameters:
+** #1. The address of a pointer to the first link of a list.
+** #2. The address of a pointer to the element to be added to the list.
 */
 
-void ft_lstadd_back(t_list **lst, t_list *new)
-{	
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
 	t_list *last;
 
-	if(*lst)
+	if (*lst)
 	{
 		last = ft_lstlast(*lst);
 		last->next = new;
