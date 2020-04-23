17060a5a3cceeb2909fc7ebecd4f214f6fb78b69
edithturn <edithpuclla20@gmail.com>
2020-04-22 23:47:09 -0700

    normientte ft_lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,29 +6,29 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/22 20:14:25 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/22 20:14:26 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/22 23:46:28 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-Deletes and frees the given element and every successor of that element, 
-using the function ’del’ and free(3).
-Finally, the pointer to the list must be set to NULL.
-Parameters:
-#1. The adress of a pointer to an element.
-#2. The adress of the function used to delete the content of the element.
+** Deletes and frees the given element and every successor of that element,
+** using the function ’del’ and free(3).
+** Finally, the pointer to the list must be set to NULL.
+** Parameters:
+** #1. The adress of a pointer to an element.
+** #2. The adress of the function used to delete the content of the element.
 */
 
-void			ft_lstclear(t_list **lst, void (*del)(void *))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list *next;
 	t_list *node;
-	
+
 	node = *lst;
-	while(node)
-	{	
+	while (node)
+	{
 		next = node->next;
 		del(node->content);
 		free(node);
