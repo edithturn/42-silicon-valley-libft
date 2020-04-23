da91a412d8648820b18dd1d3673debab1f89fda0
edithturn <edithpuclla20@gmail.com>
2020-04-23 00:08:35 -0700

    normintte lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,27 +6,27 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/22 21:23:33 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/22 21:23:35 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/23 00:07:04 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-Allocates (with malloc(3)) and returns a new
-element. The variable ’content’ is initialized
-with the value of the parameter ’content’. The
-variable ’next’ is initialized to NULL.
+** Allocates (with malloc(3)) and returns a new
+** element. The variable ’content’ is initialized
+** with the value of the parameter ’content’. The
+** variable ’next’ is initialized to NULL.
 */
 
 t_list	*ft_lstnew(void *content)
 {
-    t_list *my_list = NULL;
+	t_list	*my_list;
 
-    my_list = (t_list *) malloc(sizeof(t_list) );
-    if (my_list == NULL)
-        return NULL;
-    my_list->content = content;
+	my_list = (t_list *)malloc(sizeof(t_list));
+	if (my_list == NULL)
+		return (NULL);
+	my_list->content = content;
 	my_list->next = NULL;
-    return (my_list);
+	return (my_list);
 }
