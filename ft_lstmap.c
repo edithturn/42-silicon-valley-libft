43d92a82ff6c2f3cba0a4de0135d0fe4b72e7f17
edithturn <edithpuclla20@gmail.com>
2020-04-24 23:50:56 -0700

    fixing lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/17 22:30:53 by edith             #+#    #+#             */
-/*   Updated: 2020/04/24 23:43:46 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/24 23:47:33 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,10 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list		*current_node;
 	t_list		*next;
 
-
+	if (!lst)
+		return (NULL);
 	new_head = ft_lstnew(f(lst));
-	if (new_head == NULL)
+	if (!new_head)
 	{
 		del(f(lst));
 		return (NULL);
