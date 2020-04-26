6f7b5438d178e33c9b4de85b972ef697056c2b54
edithturn <edithpuclla20@gmail.com>
2020-04-25 18:42:20 -0700

    split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/22 21:25:56 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/25 18:35:40 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/25 18:41:55 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 char			**ft_split(char const *s, char c)
 {
-char				**new;
+	char				**ptr;
 	unsigned int		i;
 	char				*end;
+	char 			*sbt;
 
 	if (!s ||
-	(!(new = (char **)ft_memalloc(sizeof(char *) * (ft_countwords(s, c) + 1)))))
+	(!(ptr = (char **)ft_memalloc(sizeof(char *) * (ft_countwords(s, c) + 1)))))
 		return (NULL);
 	i = -1;
 	while (*s)
@@ -29,12 +30,16 @@ char				**new;
 		if (*s)
 		{
 			if (!(end = ft_strchr(s, c)))
-				new[++i] = ft_substr(s, 0, ft_strlen(s));
+			{
+				sbt = ft_substr(s, 0, ft_strlen(s));
+				ptr[i] = sbt;
+				i++;
+			}
 			else
-				new[++i] = ft_substr(s, 0, end - s);
+				ptr[++i] = ft_substr(s, 0, end - s);
 		}
 		while ((*s != c) && *s)
 			++s;
 	}
-	return (new);
+	return (ptr);
 }
