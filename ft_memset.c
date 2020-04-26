c20669c8c39bc040e8d083bf49aa89c7869d2140
edithturn <edithpuclla20@gmail.com>
2020-04-26 08:30:43 -0700

    adding null validation

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/22 21:24:47 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/25 19:18:53 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/26 08:25:00 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned	char	*ub;
 
+	if (b == NULL)
+		return NULL;
 	ub = (unsigned char *)b;
 	while (len > 0)
 	{
--- tests/test_memset.c
+++ tests/test_memset.c
@@ -6,12 +6,16 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 19:26:49 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/22 21:37:08 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/26 08:29:41 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
+/*
+**The  memset()  function  fills  the  first  n  bytes of the memory area
+**pointed to by s with the constant byte c.
+*/
 int main()
 {
 	char str1[11] = "HelloWorld";
@@ -20,8 +24,8 @@ int main()
 
 	memset(str1, c, 18*sizeof(char));
 	ft_memset(str2, c, 18*sizeof(char));
-	printf("memset: %s\n", str2);
-	printf("After memset(): %s\n", str1);
+	printf("    memset: %s\n", str2);
+	printf("ft_memset(): %s\n", str1);
 
 	return 0;
 
