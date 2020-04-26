d81b575b25df0e0eaaf5a31acf34a949d0f1fb15
edithturn <edithpuclla20@gmail.com>
2020-04-26 11:29:09 -0700

    fixing strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,17 +6,23 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 19:10:29 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/25 19:11:44 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/26 11:28:42 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+**The  strcmp()  function compares the two strings s1 and s2.  It returns
+**an integer less than, equal to, or greater than zero if  s1  is  found,
+**respectively, to be less than, to match, or be greater than s2
+*/
+
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	if(n == 0)
 		return (0);
-	while ((*s1 || *s2) && (n > 0))
+	while (((*s1 && *s2) && (n > 0)))
 	{
 		if (*s1 == *s2)
 		{
@@ -25,7 +31,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 			n--;
 		}
 		else
-			return (*(unsigned char*)s1 - *(unsigned char*)s2);
+			return ((unsigned char*)s1 - (unsigned char*)s2);
 	}
 	return (0);
 }
