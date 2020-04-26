a5bd10a323eb49b6c969a1584d88d8b98a99fa16
edithturn <edithpuclla20@gmail.com>
2020-04-25 19:19:02 -0700

    fixing memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,26 +6,21 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/22 21:24:47 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/25 19:16:18 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/25 19:18:53 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*The memset() function  fills  the  first  n  bytes of the memory area
-**pointed to by s with the constant byte c.
-*/
-
 void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned	char	*ub;
-	unsigned	char	uc;
 
 	ub = (unsigned char *)b;
-	uc = (unsigned char )c;
-	while (ub < (len + ub))
+	while (len > 0)
 	{
-		*ub = uc;
+		*ub = c;
+		len--;
 		ub++;
 	}
 	return (b);
