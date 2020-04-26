e7368683de7fa300a85563af3bb73f673d4dd4e5
edithturn <edithpuclla20@gmail.com>
2020-04-26 11:52:40 -0700

    fixing strcmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 19:10:29 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/26 11:45:50 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/26 11:52:04 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	if(n == 0)
 		return (0);
-	while ((*s1 && *s2 && (n > 0)))
+	while ((*s1++ && *s2++ && (n > 0)))
 	{
-		if (*s1 == *s2)
-		{
-			s1++;
-			s2++;
-			n--;
-		}	
+		if (*s1 != *s2)
+			break;	
+		n--;
 	}
 	return ((unsigned char*)s1 - (unsigned char*)s2);
 }
