6febb5270b80dc056309cc398cef6b6fc67b3021
edithturn <edithpuclla20@gmail.com>
2020-04-26 08:52:04 -0700

    fixing memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,24 +6,31 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/22 21:24:47 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/26 08:25:00 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/26 08:47:29 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+**The  memset()  function  fills  the  first  n  bytes of the memory area
+**pointed to by s with the constant byte c.
+*/
+
 void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned	char	*ub;
+	unsigned	char	crt;
 
 	if (b == NULL)
-		return NULL;
+		return (NULL);
 	ub = (unsigned char *)b;
+	crt = (unsigned char)c;
 	while (len > 0)
 	{
-		*ub = c;
+		*ub++ = crt;
 		len--;
-		ub++;
 	}
+	ub = '\0';
 	return (b);
 }
--- tests/test_memset.c
+++ tests/test_memset.c
@@ -6,26 +6,22 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 19:26:49 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/26 08:29:41 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/26 08:51:31 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-/*
-**The  memset()  function  fills  the  first  n  bytes of the memory area
-**pointed to by s with the constant byte c.
-*/
 int main()
 {
 	char str1[11] = "HelloWorld";
 	char str2[11] = "HelloWorld";
 	char c = 'A';
 
-	memset(str1, c, 18*sizeof(char));
-	ft_memset(str2, c, 18*sizeof(char));
-	printf("    memset: %s\n", str2);
-	printf("ft_memset(): %s\n", str1);
+	memset(str1, c, 16);
+	ft_memset(str2, c, 16);
+	printf("     memset: %s\n", str2);
+	printf("ft_memset: %s\n", str1);
 
 	return 0;
 
