229f7df8b82e94c24a4dea36c55d93ed19519659
edithturn <edithpuclla20@gmail.com>
2020-04-28 12:42:29 -0700

    fixing norm in comments

--- ft_strcat_bonus.c
+++ ft_strcat_bonus.c
@@ -6,7 +6,7 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/22 21:26:04 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/28 12:42:10 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/28 12:42:24 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /*
 **The  strcat() function appends the src string to the dest string,
-**over‐**writing the terminating null byte ('\0') at the 
+**over‐writing the terminating null byte ('\0') at the 
 **end of dest,  and  then **adds  a  terminating  null  byte.
 */
 
