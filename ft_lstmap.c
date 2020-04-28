976a61ca7e54168c4214be89350aa6da1bd2a6ef
edithturn <edithpuclla20@gmail.com>
2020-04-28 13:20:22 -0700

    fixing norme

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/17 22:30:53 by edith             #+#    #+#             */
-/*   Updated: 2020/04/28 11:53:09 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/28 13:20:14 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,12 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list		*next;
 
 	if (!lst)
-		return (NULL);
+		return (0);
 	new_head = ft_lstnew(f(lst));
 	if (!new_head)
 	{
 		del(f(lst));
-		return (NULL);
+		return (0);
 	}
 	current_node = new_head;
 	while (lst)
