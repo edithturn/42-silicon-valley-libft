338b98a8da35f49b199879fae86e6f018226d3fa
edithturn <edithpuclla20@gmail.com>
2020-04-28 13:02:35 -0700

    removing -

--- ft_strcat_bonus.c
+++ ft_strcat_bonus.c
@@ -6,16 +6,16 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/22 21:26:04 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/28 12:42:24 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/28 13:02:31 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-**The  strcat() function appends the src string to the dest string,
-**over‐writing the terminating null byte ('\0') at the 
-**end of dest,  and  then **adds  a  terminating  null  byte.
+**The strcat() function appends the src string to the dest string,
+**overwriting the terminating null byte ('\0') at the
+**end of dest,  and  then adds  a  terminating  null  byte.
 */
 
 char	*ft_strcat(char *str1, const char *str2)
