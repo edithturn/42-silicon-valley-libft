24361a4932cc50c358ad8f955e0568b8f0cbea9d
edithturn <edithpuclla20@gmail.com>
2020-04-28 00:11:38 -0700

    fixing strlcat  validating then n < destlen

--- tests/test_strlcat.c
+++ tests/test_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 17:36:53 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/22 21:40:00 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/27 23:54:33 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int main()
 {
-	char dst[] = "Hello";
-	char str[] = "Mundo";
+	char dst[] = "Destinaton";
+	char str[] = "Source";
 	int r;
-	int f;
+	/*int f;*/
 
-	f = strlcat(dst, str, 3);
-	r = ft_strlcat(dst, str, 3);
-	printf("%d\n", f);
+	/*f = strlcat(dst, str, 3);*/
+	r = ft_strlcat(dst, str, 0);
+	/*printf("%d\n", f);*/
     printf("%d", r);
 	
 	return (0);
