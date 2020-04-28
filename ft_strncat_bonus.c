ccb8be43e065fa282c7b69c2b3762f5540f2aca7
edithturn <edithpuclla20@gmail.com>
2020-04-28 13:04:44 -0700

    fixing norme

--- ft_strncat_bonus.c
+++ ft_strncat_bonus.c
@@ -6,15 +6,15 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 13:25:09 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/28 12:54:55 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/28 13:04:37 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-**The  strcat() function appends the src string 
-**to the dest string, over writing the terminating null 
+**The  strcat() function appends the src string
+**to the dest string, over writing the terminating null
 **byte ('\0') at the end of dest,  and  then
 **adds  a  terminating  null  byte.
 */
