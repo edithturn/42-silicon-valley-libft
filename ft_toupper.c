e2dd085363aa65c4d4abbe44c17a4dd7fbdc7ff3
edithturn <edithpuclla20@gmail.com>
2020-04-29 16:40:52 -0700

    modify comment

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,14 +6,15 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 14:43:35 by epuclla           #+#    #+#             */
-/*   Updated: 2020/04/26 16:19:50 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/04/29 16:40:28 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-**If c is a lowercase letter, toupper() returns its uppercase equivalent.
+**If c is a lowercase letter, toupper()
+**returns its uppercase equivalent.
 */
 
 int	ft_toupper(int c)
